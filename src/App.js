638a1302b96af35d61b0ad90ec3466b19c2e804d
Gustavo <galaxy0816tavo@gmail.com>
2024-12-18 21:19:26 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Visualizer from './Components/Visualizer/Visualizer';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Algorithm Visualizer</h1>
+        <Visualizer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Visualizer/Visualizer.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react'
+import './Visualizer.css'
+
+const Visualizer = () =>{
+    let [array, setArray] = useState([])
+
+    
+}
+
+export default Visualizer
